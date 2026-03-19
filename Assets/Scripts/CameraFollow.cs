c2b221d854d606dc6e33067237936a7c5bcadbbc
pastsawutkhon <pastsawut.khon@bumail.net>
2026-03-19 10:56:00 +0700

    camera scripts

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,15 +2,16 @@ using UnityEngine;
 
 public class CameraFollower : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Transform target;
+    public Vector3 offset = new Vector3(0, 3, -8);
+    public float smoothSpeed = 5f;
+    
+    void LateUpdate()
     {
-        
-    }
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Quaternion targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, smoothSpeed * Time.deltaTime);
     }
 }
